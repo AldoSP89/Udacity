cc778c6af3ea3bc7b1f44e5666ef9239dd642533
Arudo89 <56757869+Arudo89@users.noreply.github.com>
2019-10-19 20:47:29 +0700

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,22 +1,27 @@
+// a function to determine the height & width of the grid
 function makeGrid(height, width) {
   const table = document.getElementById("pixelCanvas");
   grid = '';
 
+  // loop to add the height of the grid
   for (h = 0; h < height; h++) {
       grid += '<tr class="row-' + h + '">';
+      // loop to add the width of the gridth
       for (w = 0; w < width; w++) {
           grid += '<td class="cell" id="row-' + h + '_cell-' + w + '"></td>';
       }
       grid += '</tr>';
   }
   table.innerHTML = grid;
-
+// calls a function to determine what happens when a cell is clicked
   colorClick();
 }
 
+// a function to determine what happens when a cell is clicked, i.e., changes the color based on the color picked in the colorPicker
 function colorClick() {
   const colorPicker = document.getElementById("colorPicker");
   const cell = document.getElementsByClassName('cell');
+  // a loop which includes an event listener specifically for click events
   for (l = 0; l < cell.length; l++) {
       cell[l].addEventListener("click", function (evt) {
           coloredCell = evt.target;
@@ -25,6 +30,7 @@ function colorClick() {
   }
 }
 
+// a function to determine what happens when the form is submitted
 function submitForm() {
   event.preventDefault();
   const height = document.getElementById('inputHeight').value;
@@ -32,6 +38,7 @@ function submitForm() {
   makeGrid(height, width);
 }
 
+// calls the submitForm function
 document.getElementById('sizePicker').onsubmit = function () {
   submitForm();
 };
